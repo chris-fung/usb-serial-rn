ce0857c8353eb476787d56375d3bb7a511fc3ee6
chris fung <2142786@qq.com>
2024-02-01 15:16:37 +0800

    feat: 版本升级

---
+++ android/src/main/java/com/melihyarikkaya/rnserialport/XzjhModule.java
@@ -0,0 +1,81 @@
+package com.melihyarikkaya.rnserialport;
+
+import android.app.PendingIntent;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.LifecycleEventListener;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableNativeArray;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.net.Network;
+import android.util.Base64;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.io.IOException;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+
+import android.app.XzjhSystemManager
+
+public class XzjhModule extends ReactContextBaseJavaModule {
+  public static final String TAG = "XzjhModule";
+  XzjhSystemManager mMamager = null;
+
+  public XzjhModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    mMamager = (XzjhSystemManager)getSystemService("xzjh_server");
+  }
+
+  @Override
+  public String getName() {
+    return TAG;
+  }
+
+  @ReactMethod
+  public void setVerticalScreen() {
+      // 调用 jar 中的方法并返回结果
+      mMamager.xzjhSetScreenRotation(1)
+  }
+}
